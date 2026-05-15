132079b3ab0cfe24ac8fd75e768a2097a3655dc3
Sambca-hub <maymolajith518@gmail.com>
2026-05-15 21:20:15 +0530

    Update Home.jsx

--- aroma/src/component/Home.jsx
+++ aroma/src/component/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
     
     <div className='hero'>
         <center><h1>Welcome to Aroma Cafe</h1>
         <p>Experience the best coffee in town with us!</p>
-        <button className='btn' onClick={() => window.location.href = '/menu'}>Explore Menu</button>    
+        <button className='btn' onClick={() => navigate('/menu')}>Explore Menu</button>    
         </center> 
     </div>
   )
